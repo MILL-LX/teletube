d5d700197f1360f40ad39b9207882cf0ea558af8
MILL-LX <info@mill.pt>
2026-05-14 18:25:23 +0100

    writes to screen but rotated

--- app/src/teletube/app.py
+++ app/src/teletube/app.py
@@ -1,10 +1,24 @@
-from dotenv import load_dotenv
+import pygame
+import pygame._freetype
+pygame.init()
+pygame._freetype.init()
 
-load_dotenv()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+W, H = screen.get_size()
 
-def main():
-    print("Hello from app!")
+font = pygame._freetype.Font(None, 72)
 
+def show_message(text, bg=(0,0,0), fg=(255,255,255)):
+    screen.fill(bg)
+    text_surf, rect = font.render(text, fg)
+    rect.center = (W // 2, H // 2)
+    screen.blit(text_surf, rect)
+    pygame.display.flip()
 
-if __name__ == "__main__":
-    main()
+show_message("Hello, Pi!")
+
+while True:
+    for e in pygame.event.get():
+        if e.type in (pygame.QUIT, pygame.KEYDOWN):
+            pygame.quit()
+            raise SystemExit
